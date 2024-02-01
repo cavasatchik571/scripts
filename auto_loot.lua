b01cbfe0fdf485df325e81fd2fbecb281ff862fb
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-02-01 16:17:51 +0200

    Update auto_loot.lua

--- auto_loot.lua
+++ auto_loot.lua
@@ -3,6 +3,7 @@ local env = (getgenv or function() end)() or _ENV or shared or _G or {}
 local new_auto_loot = not env.auto_loot and true or nil
 env.auto_loot = new_auto_loot
 if not new_auto_loot then return end
+local current_camera = workspace.CurrentCamera
 local current_rooms = workspace:WaitForChild('CurrentRooms')
 local render_stepped = game:GetService('RunService').RenderStepped
 
@@ -11,7 +12,9 @@ while env.auto_loot do
 	
 	for idx = 1, #descendants do
 		local descendant = descendants[idx]
-		if descendant.Name ~= 'LootPrompt' or not descendant:IsA('ProximityPrompt') then continue end
+		if descendant.Name ~= 'LootPrompt' or not descendant:IsA('ProximityPrompt') or
+			(current_camera.CFrame.Position - descendant.Parent:GetPivot().Position).Magnitude > 32 then continue end
+		
 		fireproximityprompt(descendant)
 	end
 	
