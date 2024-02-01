74cc343045d9e87111c3fb863c3af31d2a59921a
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-02-01 15:41:28 +0200

    Create auto_loot.lua

---
+++ auto_loot.lua
@@ -0,0 +1,22 @@
+if game.PlaceId ~= 6839171747 then return end
+local env = (getgenv or function() end)() or _ENV or shared or _G or {}
+local new_auto_loot = not env.auto_loot and true or nil
+env.auto_loot = new_auto_loot
+if not new_auto_loot then return end
+local current_rooms = workspace:WaitForChild('CurrentRooms')
+local render_stepped = game:GetService('RunService').RenderStepped
+
+while env.auto_loot do
+	local descendants = current_rooms:GetDescendants()
+	
+	for idx = 1, #descendants do
+		local descendant = descendants[idx]
+		if descendant.Name ~= 'LootPrompt' or not descendant:IsA('ProximityPrompt') then continue end
+		descendant.Enabled = true
+		descendant.HoldDuration = 0
+		descendant.MaxActivationDistance = 64
+		fireproximityprompt(descendant)
+	end
+	
+	render_stepped:Wait()
+end
