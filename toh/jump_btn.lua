39df1b9a9ebc9cea552f5054d0612d4d53eed69b
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-04-23 04:30:49 +0300

    Update jump_btn.lua

--- toh/jump_btn.lua
+++ toh/jump_btn.lua
@@ -1,1 +1,4 @@
-
+game.Players.LocalPlayer.PlayerGui.MobileShiftLock.BottomLeftControl.MouseLockLabel.Position = UDim2.fromScale(-1, -1)
+game.Players.LocalPlayer.PlayerGui.MobileShiftLock.BottomLeftControl.MouseLockLabel.Size = UDim2.fromOffset(100, 100)
+game.Players.LocalPlayer.PlayerGui.TouchGui.TouchControlFrame.JumpButton.Position = UDim2.fromScale(0.8, 0.5)
+game.Players.LocalPlayer.PlayerGui.TouchGui.TouchControlFrame.JumpButton.Size = UDim2.fromOffset(150, 150)
