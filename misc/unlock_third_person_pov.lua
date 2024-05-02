4d6776f95d8ad99ba55387d582f36b19dad56230
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-05-02 16:11:18 +0300

    Update unlock_third_person_pov.lua

--- misc/unlock_third_person_pov.lua
+++ misc/unlock_third_person_pov.lua
@@ -1,4 +1,9 @@
-local plr = game.Players.LocalPlayer
+-- source code
+
+local plr = game:GetService('Players').LocalPlayer
+
+-- logic
+
 plr.CameraMinZoomDistance = 0.5
 plr.CameraMaxZoomDistance = 10
 plr.CameraMode = Enum.CameraMode.Classic
