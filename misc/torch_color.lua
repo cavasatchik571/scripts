3f75d4ec8e43602c51250eedc9104e784243befb
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-06-03 22:06:43 +0300

    Update torch_color.lua

--- misc/torch_color.lua
+++ misc/torch_color.lua
@@ -1,1 +1,1 @@
-game:GetService('ReplicatedStorage'):WaitForChild('RemoteEvent', 4):FireServer('paychangethetorchcolor', Color3.new(0, .4984, 0)) ---4
+game:GetService('ReplicatedStorage'):WaitForChild('RemoteEvent', 4):FireServer('paychangethetorchcolor', Color3.new(0, .2514, 0)) ---4
