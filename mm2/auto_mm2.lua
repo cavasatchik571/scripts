66fafd9f060d8f1fa7203738d3a839d6b1b9ddaf
cavasatchik571 <158255806+cavasatchik571@users.noreply.github.com>
2024-06-26 19:34:29 +0300

    Update auto_mm2.lua

--- mm2/auto_mm2.lua
+++ mm2/auto_mm2.lua
@@ -1,49 +1,59 @@
--- source code
+--!nolint
+--!nonstrict
 
-local place_id = 142823291
-if game.PlaceId ~= place_id then return end
-local code = `loadstring(game:HttpGet('https://raw.githubusercontent.com/cavasatchik571/scripts/main/mm2/auto_mm2.lua', true))()`
-local env = (getgenv or function() end)() or _ENV or shared or _G
-if not env.MF_injected then (queueonteleport or queue_on_teleport)(code) end
-env.MF_injected = true
-local new_enabled = not env.MF and true or nil
-env.MF = new_enabled
-if not new_enabled then return end
+local _4 = Color3.new(0, .4984, 0)
 
-local offset = Vector3.new(0, -2, 0)
-local speed = 20.14
+-- by @Vov4ik4124
 
-local cf_new = CFrame.new
-local cf_yxz = CFrame.fromEulerAnglesYXZ
-local clear = table.clear
-local defer = task.defer
-local pi = math.pi
+local place_id = game.PlaceId
+if place_id ~= 142823291 then return end
+local fti = firetouchinterest or fire_touch_interest
 local plrs = game:GetService('Players')
-local ps = game:GetService('RunService').PreSimulation
+local qot = queueonteleport or queue_on_teleport
+local you = plrs.LocalPlayer
+local your_name = you.Name
+if not fti or not qot then return you:Kick('AFK4 doesn\'t support your executor') end
+local env = shared or _G
+if env.afk4 then return end
+env.afk4 = true
+qot('loadstring(game:HttpGet(\'https://raw.githubusercontent.com/cavasatchik571/scripts/main/mm2/auto_mm2.lua\', true))()')
+local all = Enum.CoreGuiType.All
+local check = 0
+local cf_new = CFrame.new
+local dead = Enum.HumanoidStateType.Dead
+local lighting = game:GetService('Lighting')
+local min = math.min
 local remove = table.remove
 local rng = Random.new()
-local round = math.round
 local sleep = task.wait
 local sort = table.sort
+local sound_service = game:GetService('SoundService')
 local starter_gui = game:GetService('StarterGui')
-local step = 4
+local string_find = string.find
+local table_find = table.find
 local ts = game:GetService('TeleportService')
 local ts_ttpi = ts.TeleportToPlaceInstance
-local you = plrs.LocalPlayer
+local upper = string.upper
 local vec2_zero = Vector2.zero
+local vec3_new = Vector3.new
 local vec3_zero = Vector3.zero
 local vu = game:GetService('VirtualUser')
 local vu_b1d = vu.Button1Down
 local vu_b1u = vu.Button1Up
+local nearest_plr_pos, point_of_interest = vec3_zero, vec3_zero
 
-local _set = function(a, b, c) a[b] = c end
-local fti = firetouchinterest or fire_touch_interest or function(p0, p1, uint)
-	if uint then warn('UInt is unsupported') end
-	local fct0, fct1, fp0 = p0.CanTouch, p1.CanTouch, p0.Position
-	p0.CanTouch, p1.CanTouch, p0.Position = true, true, p1.Position
-	defer(_set, p0, 'CanTouch', fct0)
-	defer(_set, p1, 'CanTouch', fct1)
-	defer(_set, p0, 'Position', fp0)
+local offset_pos, speed, speed_lb, speed_ub = vec3_new(0, -2, 0), 20.14, -4, 0
+
+---4 with/💚
+
+local function clear_velocity(inst)
+	local children = inst:GetChildren()
+	for i = 1, #children do
+		local child = children[i]
+		if not child:IsA('BasePart') then continue end
+		child.AssemblyAngularVelocity = vec3_zero
+		child.AssemblyLinearVelocity = vec3_zero
+	end
 end
 
 local function filter_coins(cc)
@@ -57,76 +67,49 @@ local function filter_coins(cc)
 	end
 end
 
-local function get_is_alive(plr)
-	if typeof(plr) ~= 'Instance' or not plr:IsA('Player') or not plr:FindFirstChildOfClass('Backpack') then return false end
+local function is_alive(plr)
 	local bp = plr:FindFirstChildOfClass('Backpack')
 	if not bp then return false end
 	local char = plr.Character
-	if not char then return false end
+	if not char or not char.Parent then return false end
 	local h = char:FindFirstChildOfClass('Humanoid')
-	if not h or h.Health <= 0 or h:GetState().Value == 15 or not h.RootPart then return false end
+	if not h or h.Health <= 0 or h:GetState() == dead or not h.RootPart then return false end
 	return true
 end
 
-local function get_stepped(e)
-	if typeof(e) ~= 'number' then return 0 end
-	return step == 0 and e or (round(e / step) * step)
-end
-
-local function get_threat()
-	local char = you.Character
-	local char_pos = char:GetPivot().Position
-	--local item_name = (you:FindFirstChildOfClass('Backpack'):FindFirstChild('Knife') or char:FindFirstChild('Knife')) and 'Gun' or 'Knife'
+local function near_plr()
+	local dist = 10000
 	local list = plrs:GetPlayers()
-	local radius = 400
+	local pos = you.Character:FindFirstChildOfClass('Humanoid').RootPart.Position
 	local result
 	for i = 1, #list do
 		local element = list[i]
-		if element == you or not get_is_alive(element) then continue end
-		local other_char = element.Character
-		--if not element:FindFirstChildOfClass('Backpack'):FindFirstChild(item_name) and not other_char:FindFirstChild(item_name) then continue end
-		local dist = (char_pos - other_char:GetPivot().Position).Magnitude
-		if dist > radius then continue end
-		radius, result = dist, element
+		if element == you or not is_alive(element) then continue end
+		local new_dist = (element.Character:FindFirstChildOfClass('Humanoid').RootPart.Position - pos).Magnitude
+		if new_dist >= dist then continue end
+		dist = new_dist
+		result = element
 	end
-	clear(list)
 	return result
 end
 
-local function reset_velocity(inst)
-	local list = inst:GetDescendants()
-	for i = 1, #list do
-		local element = list[i]
-		if not element:IsA('BasePart') then continue end
-		element.AssemblyAngularVelocity, element.AssemblyLinearVelocity = vec3_zero, vec3_zero
-		element.CanCollide, element.CanQuery = false, false
+local function remove_all_except(inst, ...)
+	local args = {...}
+	local children = inst:GetChildren()
+	for i = 1, #children do
+		local child = children[i]
+		if table_find(args, child.Name) then continue end
+		child:Destroy()
 	end
-	clear(list)
 end
 
 local function sort_coins(a, b)
-	local ap, bp = a.Position or vec3_zero, b.Position or vec3_zero
-	local p0 = you.Character:GetPivot().Position
-	local score_a, score_b = -(ap - p0).Magnitude, -(bp - p0).Magnitude
-	local threat = get_threat()
-	if threat then
-		local p1 = threat.Character:GetPivot().Position
-		score_a += get_stepped((ap - p1).Magnitude) / 4
-		score_b += get_stepped((bp - p1).Magnitude) / 4
-	end
-	return score_a > score_b
+	local a_score = -(point_of_interest - a.Position).Magnitude + (nearest_plr_pos - a.Position).Magnitude
+	local b_score = -(point_of_interest - b.Position).Magnitude + (nearest_plr_pos - b.Position).Magnitude
+	return a_score > b_score
 end
 
--- logic
-
-local connection_0 = you.Idled:Connect(function()
-	if not env.MF then return end
-	pcall(vu_b1d, vu, vec2_zero)
-	ps:Wait()
-	pcall(vu_b1u, vu, vec2_zero)
-end)
-
-local connection_1 = you.ChildRemoved:Connect(function(child)
+you.ChildRemoved:Connect(function(child)
 	if not child:IsA('PlayerScripts') then return end
 	while true do
 		sleep(1)
@@ -134,82 +117,117 @@ local connection_1 = you.ChildRemoved:Connect(function(child)
 	end
 end)
 
-starter_gui:SetCore('SendNotification', {Button1 = 'OK', Duration = 4, Title = 'MM2', Text = 'Auto farm script has been activated.'})
-while env.MF do
-	local dt = ps:Wait()
-	step = rng:NextNumber(4, 6)
-	if not get_is_alive(you) then continue end
-	local bp = you:FindFirstChildOfClass('Backpack')
-	local char = you.Character
-	local h = char:FindFirstChildOfClass('Humanoid')
-	local hrp = h.RootPart
-	h.PlatformStand = false
-	local map = workspace:FindFirstChild('Normal')
-	if not map then workspace.Gravity = 196.2 continue end
-	local cc = map:FindFirstChild('CoinContainer')
-	if not cc then workspace.Gravity = 196.2 continue end
-	local children = map:GetChildren()
-	for i = 1, #children do
-		local child = children[i]
-		if child == cc then continue end
-		child:Destroy()
-	end
-	clear(children)
-	reset_velocity(char)
-	local particle_emitter
-	local descendants = cc:GetDescendants()
-	for i = 1, #descendants do
-		local descendant = descendants[i]
-		if not descendant:IsA('ParticleEmitter') or descendant.Texture ~= 'rbxassetid://16885815956' or
-			descendant.Parent:FindFirstChildWhichIsA('MeshPart').Transparency > 0 then continue end
-		particle_emitter = descendant
-		break
-	end
-	clear(descendants)
-	if particle_emitter then
-		hrp.CFrame = particle_emitter.Parent:GetPivot()
-		continue
-	else
-		if bp:FindFirstChild('Knife') or char:FindFirstChild('Knife') then
-			local function tp_to_safe_zone()
-				local init = hrp:GetAttribute('SafeCFrame')
-				if not init then
-					init = hrp.CFrame + vec3_new(0, 200, 0)
-					hrp:SetAttribute('SafeCFrame', init)
-				end
-				hrp.CFrame = init
-			end
+you.Idled:Connect(function()
+	if not env.afk4 then return end
+	pcall(vu_b1d, vu, vec2_zero)
+	sleep()
+	pcall(vu_b1u, vu, vec2_zero)
+end)
 
-			pcall(function() if you.PlayerGui.MainGUI.Game.CoinBags.Container.Coin.Full.Visible then tp_to_safe_zone() end end)
-			pcall(function() if you.PlayerGui.MainGUI.Lobby.Dock.CoinBags.Container.Coin.Full.Visible then tp_to_safe_zone() end end)
-		else
-			pcall(function() if you.PlayerGui.MainGUI.Game.CoinBags.Container.Coin.Full.Visible then h.Health = 0 end end)
-			pcall(function() if you.PlayerGui.MainGUI.Lobby.Dock.CoinBags.Container.Coin.Full.Visible then h.Health = 0 end end)
+local sg = game:GetService('StarterGui')
+local sg_sc = sg.SetCore
+local sg_scp = {Button1 = 'OK', Duration = 4, Icon = 'rbxassetid://7440784829', Text = 'Script activated', Title = 'AFK4'}
+while true do if pcall(sg_sc, sg, 'SendNotification', sg_scp) then break else sleep(0.04) end end
+while true do
+	local dt = sleep()
+	local list = plrs:GetPlayers()
+	for i = 1, #list do
+		local plr = list[i]
+		list[i] = plr.Name
+		local char = plr.Character
+		if not char then continue end
+		local children = char:GetChildren()
+		for j = 1, #children do
+			local child = children[j]
+			if child:IsA('Humanoid') then continue end
+			local name = child.Name
+			if name == 'Head' or name == 'HumanoidRootPart' or name == 'LowerTorso' or name == 'Torso' or name == 'UpperTorso' then
+				remove_all_except(child, 'Neck', 'Root', 'RootJoint', 'Waist', 'WaistJoint')
+				child.Transparency = 1
+				continue
+			end
+			child:Destroy()
 		end
 	end
-	local coins = cc:GetChildren()
-	local p0 = hrp:GetPivot().Position
-	filter_coins(coins)
-	if #coins <= 0 then continue end
-	sort(coins, sort_coins)
-	local coin = coins[1]
-	clear(coins)
-	local p1 = coin.Position
-	local diff = p1 + offset - p0
-	local dist = diff.Magnitude
-	if dist > 666 then
-		hrp.CFrame = coin.CFrame
-	else
-		local pos = p0 + (diff.Magnitude == 0 and vec3_zero or diff.Unit) * dt * (speed - rng:NextNumber(0, 4))
-		hrp.CFrame = cf_new(pos) * cf_yxz(pi, select(2, cf_new(pos, p1).Rotation:ToEulerAnglesYXZ()), 0)
+	remove_all_except(workspace, 'Camera', 'Normal', 'Terrain', unpack(list))
+	lighting:ClearAllChildren()
+	sound_service:ClearAllChildren()
+	starter_gui:SetCoreGuiEnabled(all, false)
+	if not is_alive(you) then continue end
+	local your_gui = you:FindFirstChildOfClass('PlayerGui')
+	if not your_gui then continue end
+	local char = you.Character
+	clear_velocity(char)
+	local h = char:FindFirstChildOfClass('Humanoid')
+	h.PlatformStand = true
+	workspace.Gravity = 0
+	clear_velocity(char)
+	local map = workspace:FindFirstChild('Normal')
+	if not map then you:SetAttribute('4', nil) continue end
+	remove_all_except(map, 'CoinContainer')
+	local cc = map:FindFirstChild('CoinContainer')
+	if not cc then continue end
+	local hrp = h.RootPart
+	if you:GetAttribute('4') == _4 then
+		if you:FindFirstChildOfClass('Backpack'):FindFirstChild('Knife') or char:FindFirstChild('Knife') then
+			local init = hrp:GetAttribute('SafeCFrame')
+			if not init then
+				init = hrp.CFrame + vec3_new(0, 240, 0)
+				hrp:SetAttribute('SafeCFrame', init)
+			end
+			hrp.CFrame = init
+		else
+			h.Health = 0
+		end
+		continue
 	end
-	h.PlatformStand, workspace.Gravity = true, 0
-	fti(hrp, coin, 1)
-	fti(hrp, coin, 0)
+	if check >= 40 then
+		check = 0
+		local descendants = your_gui:GetDescendants()
+		for i = 1, #descendants do
+			local descendant = descendants[i]
+			if not descendant:IsA('TextLabel') or not descendant.Visible or not string_find(upper(descendant.Text), 'FULL') then continue end
+			local succ = true
+			while descendant ~= your_gui do
+				if descendant:IsA('GuiObject') and not descendant.Visible then
+					succ = false
+					break
+				end
+				descendant = descendant.Parent
+			end
+			if not succ then continue end
+			you:SetAttribute('4', _4)
+			break
+		end
+	else
+		check += 1
+	end
+	if you:GetAttribute('4') == _4 then continue end
+	local plr = near_plr()
+	if not plr or not is_alive(plr) then continue end
+	local list = cc:GetChildren()
+	filter_coins(list)
+	local len = #list
+	if len == 0 then continue end
+	local p0 = hrp.Position
+	local p1 = list[1]:GetPivot().Position
+	nearest_plr_pos = plr.Character:FindFirstChildOfClass('Humanoid').RootPart.Position
+	point_of_interest = p0
+	sort(list, sort_coins)
+	local diff = p1 + offset_pos - p0
+	local dist = diff.Magnitude
+	if dist <= 0.2444 then
+		continue
+	elseif dist >= 666 then
+		hrp.CFrame = cf_new(offset_pos + p1, p1)
+	else
+		local pos = p0 + (dist == 0 and vec3_zero or diff.Unit) * (speed + rng:NextNumber(speed_lb, speed_ub)) * dt
+		hrp.CFrame = cf_new(pos) * cf_new(offset_pos + p1, p1).Rotation
+	end
+	for i = 1, len do
+		local part = list[i]
+		if (hrp.Position - part.Position).Magnitude >= 8 then continue end
+		fti(hrp, part, 1)
+		fti(hrp, part, 0)
+	end
 end
-
-starter_gui:SetCore('SendNotification', {Button1 = 'OK', Duration = 4, Title = 'MM2', Text = 'Auto farm script has been deactivated.'})
-connection_0:Disconnect()
-connection_1:Disconnect()
-if not get_is_alive(you) then return end
-you.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
