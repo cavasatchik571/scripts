c1efb2ec393404c9a5a6143ba4b72a317e40cbef
Absolutely No-one <158255806+cavasatchik571@users.noreply.github.com>
2024-07-25 08:51:58 +0300

    Update auto_mm2.lua

--- mm2/auto_mm2.lua
+++ mm2/auto_mm2.lua
@@ -18,7 +18,7 @@ local qt = queueonteleport or (syn and syn.queue_on_teleport) or queue_on_telepo
 local you = plrs.LocalPlayer
 if not fti or not gncm or not hf or not hmm or not ncc or not qt then return you:Kick('Your client doesn\'t support AFK4') end
 
--- fail-safe measure
+-- fail-safe measures
 
 local sleep = task.wait
 local ts = game:GetService('TeleportService')
@@ -55,120 +55,129 @@ end)
 -- states
 
 local hst = Enum.HumanoidStateType
+local hst_al = 1
 local hst_approved = {hst.Freefall}
-local hst_al = #hst_approved
-local hst_ignore = hst:GetEnumItems()
-local hst_il = #hst_ignore
-local remove = table.remove
-local table_find = table.find
-for i = hst_il, 1, -1 do
-	if table_find(hst_approved, hst_ignore[i]) then
-		remove(hst_ignore, i)
-		hst_il -= 1
-	end
-end
+local hst_dead = hst.Dead
+local hst_el = 15
+local hst_exclude = {
+	hst.FallingDown, hst.Ragdoll, hst.GettingUp, hst.Jumping, hst.Swimming, hst.Flying, hst.Landed, hst.Running,
+	hst.RunningNoPhysics, hst.StrafingNoPhysics, hst.Climbing, hst.Seated, hst.PlatformStanding, hst_dead, hst.Physics
+}
 
 -- hooks
 
+local find = table.find
+local inst_new = Instance.new
 local your_h
 do
-	local inst_new = Instance.new
-	local old_gse, old_hmm, old_sse
+	local is_hst = function(e) return typeof(e) == 'EnumItem' and e.EnumType == hst end
+	local old_gse, old_nc, old_sse
 	local test_h = inst_new('Humanoid')
-	local function is_hst(e)
-		return typeof(e) == 'EnumItem' and e.EnumType == hst
-	end
 	old_gse = hf(test_h.GetStateEnabled, ncc(function(self, arg_1, ...)
 		if self ~= your_h or not is_hst(arg_1) then return old_gse(self, arg_1, ...) end
-		return if table_find(hst_approved, arg_1) then true else false
+		return if find(hst_approved, arg_1) then true else false
 	end))
-	old_hmm = hmm(game, '__namecall', ncc(function(self, arg_1, ...)
-		if self == your_h and is_hst(arg_1)then
+	old_nc = hmm(game, '__namecall', ncc(function(self, arg_1, ...)
+		if self == your_h and is_hst(arg_1) then
 			local ncm = gncm()
 			if ncm == 'GetStateEnabled' then
-				return if table_find(hst_approved, arg_1) then true else false
+				return if find(hst_approved, arg_1) then true else false
 			elseif ncm == 'SetStateEnabled' then
-				return old_hmm(self, arg_1, if table_find(hst_approved, arg_1) then true else false)
+				return old_nc(self, arg_1, if find(hst_approved, arg_1) then true else false)
 			end
 		end
-		return old_hmm(self, arg_1, ...)
+		return old_nc(self, arg_1, ...)
 	end))
 	old_sse = hf(test_h.SetStateEnabled, ncc(function(self, arg_1, ...)
 		if self ~= your_h or not is_hst(arg_1) then return old_sse(self, arg_1, ...) end
-		return old_sse(self, arg_1, if table_find(hst_approved, arg_1) then true else false)
+		return old_sse(self, arg_1, if find(hst_approved, arg_1) then true else false)
 	end))
 	test_h:Destroy()
 end
 
+-- set-up
+
+local create = coroutine.create
+local huge = math.huge
+local resume = coroutine.resume
+local vec3_power = Vector3.new(huge, huge, huge)
+local zero = Vector3.zero
+
+local function char_added(char)
+	if not char then return end
+	local new_h = char:WaitForChild('Humanoid', 40.4)
+	if not char or not char.Parent or not new_h or not new_h.Parent then return end
+	for i = 1, hst_el do new_h:SetStateEnabled(hst_exclude[i], false) end
+	for i = 1, hst_al do
+		local state = hst_approved[i]
+		new_h:SetStateEnabled(state, true)
+		if state == hst_dead then continue end
+		new_h:ChangeState(state)
+	end
+	local your_hrp = char:WaitForChild('HumanoidRootPart', 40.4)
+	if not char or not char.Parent or not your_hrp or not your_hrp.Parent then return end
+	local bav = inst_new('BodyAngularVelocity')
+	bav.AngularVelocity = zero
+	bav.MaxTorque = vec3_power
+	bav.Name = 'BAV'
+	bav.Parent = your_hrp
+	local bv = inst_new('BodyVelocity')
+	bv.MaxForce = vec3_power
+	bv.Name = 'BV'
+	bv.Velocity = zero
+	bv.Parent = your_hrp
+	your_h = new_h
+end
+
 -- logic
 
+local added_at = huge
 local clear = table.clear
 local coins = {}
-local dead = hst.Dead
-local vec3_new = Vector3.new
-local zero = Vector3.zero
-local function cc0(child) coins[#coins + 1] = child end
-local function cc1(child)
-	local i = table_find(coins, child)
+local remove = table.remove
+
+local function is_coin_valid(e)
+	return (e.Parent or game).Name == 'CoinContainer' and e.Name == 'Coin_Server' and
+		e:FindFirstChild('CoinVisual') and not e:GetAttribute('Collected')
+end
+
+local function cc_child_removed(child)
+	local i = find(coins, child)
 	if not i then return end
 	remove(coins, i)
 end
 
+local function cc_child_added(child)
+	sleep(0.01)
+	if not is_coin_valid(child) then return end
+	coins[#coins + 1] = child
+	local c0, c1, c2
+	local function clean_up()
+		if is_coin_valid(child) then return end
+		c0:Disconnect()
+		c1:Disconnect()
+		c2:Disconnect()
+		cc_child_removed(child)
+	end
+	c0 = child.Changed:Connect(clean_up)
+	c1 = child.ChildRemoved:Connect(clean_up)
+	c2 = child:GetAttributeChangedSignal('Collected'):Once(clean_up)
+end
+
 local function descendant_added(e)
-	local parent = e.Parent
-	if e.Name == 'CoinContainer' then
+	if not e.Parent then return end
+	local name = e.Name
+	if name == 'CoinContainer' then
+		added_at = tick()
 		clear(coins)
+		e.ChildAdded:Connect(cc_child_added)
+		e.ChildRemoved:Connect(cc_child_removed)
 		local list = e:GetChildren()
-		for i = 1, #list do coins[i] = list[i] end
-		e.ChildAdded:Connect(cc0)
-		e.ChildRemoved:Connect(cc1)
-	elseif e:IsA('BasePart') and e ~= (if your_h then your_h.RootPart else nil) and parent.Name ~= 'CoinContainer' then
+		for i = 1, #list do cc_child_added(list[i]) end
+		clear(list)
+	elseif e:IsA('BasePart') and e ~= (if your_h then your_h.RootPart else nil) and name ~= 'Coin_Server' then
 		e.CanTouch = false
 	end
-end
-
-workspace.DescendantAdded:Connect(descendant_added)
-workspace.DescendantRemoving:Connect(function(e)
-	if e.Name ~= 'CoinContainer' then return end
-	clear(coins)
-	you:SetAttribute('Done', nil)
-end)
-
-do
-	local list = workspace:GetDescendants()
-	for i = 1, #list do descendant_added(list[i]) end
-end
-
----4💚
-
-local all = Enum.CoreGuiType.All
-local checks = 0
-local lighting = game:GetService('Lighting')
-local rng = Random.new()
-local ss = game:GetService('SoundService')
-local string_find = string.find
-local upper = string.upper
-
-local function full_bag_of(main_gui, t)
-	if not main_gui or not t then return false end
-	local descendants = main_gui:GetDescendants()
-	local ut = upper(tostring(t))
-	if ut == '' then return false end
-	for i = 1, #descendants do
-		if i % 140 == 0 then sleep() end
-		local descendant = descendants[i]
-		if not descendant:IsA('TextLabel') or not descendant.Visible or
-			string_find(upper(descendant.Text), 'FULL', 1, false) ~= 1 then continue end
-		local parent = descendant.Parent
-		if not string_find(upper(parent.Name), ut) then continue end
-		local succ = true
-		while parent ~= main_gui do
-			if (parent:IsA('GuiObject') and not parent.Visible) or parent:IsA('LocalScript') then succ = false break end
-			parent = parent.Parent
-		end
-		if succ then return true end
-	end
-	return false
 end
 
 local function remove_all_except(inst, ...)
@@ -176,65 +185,54 @@ local function remove_all_except(inst, ...)
 	local children = inst:GetChildren()
 	for i = 1, #children do
 		local child = children[i]
-		if table_find(args, child.Name) then continue end
+		if find(args, child.Name) then continue end
 		child:Destroy()
 	end
+	clear(args)
 end
 
-local function set_cf(part, cf)
-	local pos = cf.Position
-	part.Velocity = zero
-	part.RotVelocity = zero
-	part.Position = pos
-	part.CFrame = cf
-	part.CanTouch = true
-	part.CanCollide = false
-	part.AssemblyLinearVelocity = zero
-	part.AssemblyAngularVelocity = zero
-end
-
-coroutine.resume(coroutine.create(function()
-	while true do
-		sleep(0.04)
-		if you:GetAttribute('Done') or
-			not your_h or
-			your_h.Health <= 0 or
-			your_h:GetState() == dead or
-			not workspace:FindFirstChild('Normal') then continue end
-
-		local char = you.Character
-		local h = char:FindFirstChildOfClass('Humanoid')
-		local hrp = h.RootPart
-		local your_gui = you:FindFirstChildOfClass('PlayerGui')
-		if not your_gui then continue end
-		if checks >= 8 then
-			checks = 0
-			local succ, result = pcall(full_bag_of, your_gui:FindFirstChild('MainGUI'), 'Coin')
-			if not succ or not result then continue end
-			you:SetAttribute('Done', true)
-			if you:FindFirstChildOfClass('Backpack'):FindFirstChild('Knife') or char:FindFirstChild('Knife') then
-				local init = hrp:GetAttribute('SafeCFrame')
-				if not init then
-					init = hrp.CFrame + vec3_new(0, 240, 0)
-					hrp:SetAttribute('SafeCFrame', init)
-				end
-				hrp.CFrame = init
-			else
-				h.Health = 0
-			end
-		else
-			checks += 1
-		end
+local function reset_velocity(char)
+	local descendants = char:GetDescendants()
+	for i = 1, #descendants do
+		local descendant = descendants[i]
+		if not descendant:IsA('BasePart') then continue end
+		descendant.AssemblyAngularVelocity = zero
+		descendant.AssemblyLinearVelocity = zero
+		descendant.CanCollide = false
+		descendant.CanQuery = false
+		descendant.CanTouch = descendant.Name == 'HumanoidRootPart'
+		descendant.RotVelocity = zero
+		descendant.Velocity = zero
 	end
-end))
+	clear(descendants)
+end
+
+workspace.DescendantAdded:Connect(descendant_added)
+workspace.DescendantRemoving:Connect(function(e)
+	if e.Name ~= 'CoinContainer' then return end
+	added_at = huge
+	clear(coins)
+	you:SetAttribute('Done', nil)
+end)
 
 local sg = game:GetService('StarterGui')
 do
+	you.CharacterAdded:Connect(char_added)
+	local char = you.Character
+	if char then resume(create(char_added), char) end
+	local list = workspace:GetDescendants()
+	for i = 1, #list do resume(create(descendant_added), list[i]) end
 	local sg_sc = sg.SetCore
 	local sg_scp = {Button1 = 'OK', Duration = 4, Icon = 'rbxassetid://7440784829', Text = 'Script activated', Title = 'AFK4'}
 	while true do if pcall(sg_sc, sg, 'SendNotification', sg_scp) then break else sleep(0.04) end end
-	clear(sg_scp)
 end
+
+local all = Enum.CoreGuiType.All
+local cf_new = CFrame.new
+local lighting = game:GetService('Lighting')
+local rng = Random.new()
+local safe_pos = cf_new(0, -240, 0)
+local ss = game:GetService('SoundService')
 
 while true do
 	local list = plrs:GetPlayers()
@@ -261,43 +259,45 @@ while true do
 	sg:SetCoreGuiEnabled(all, false)
 	ss:ClearAllChildren()
 	workspace.Gravity = 0
-	local map = workspace:FindFirstChild('Normal')
-	if map then remove_all_except(map, 'CoinContainer') end
 	local char = you.Character
-	if not char then sleep(0.04) continue end
-	your_h = char:FindFirstChildOfClass('Humanoid')
-	if not your_h then sleep(0.04) continue end
-	if not your_h:GetAttribute('Done') then
-		your_h:SetAttribute('Done', true)
-		local sse = your_h.SetStateEnabled
-		for i = 1, hst_il do pcall(sse, your_h, hst_ignore[i], false) end
-		for i = 1, hst_al do
-			local state = hst_approved[i]
-			pcall(sse, your_h, state, true)
-			if state == dead then continue end
-			your_h:ChangeState(state)
-		end
-	end
-	if not your_h or your_h.Health <= 0 or your_h:GetState() == dead then sleep(0.04) continue end
+	if not char or not your_h or not your_h.Parent or your_h.Health <= 0 or your_h:GetState() == hst_dead then sleep(0.04) continue end
 	local rp = your_h.RootPart
 	if not rp or not rp.Parent then sleep(0.04) continue end
+	local map = workspace:FindFirstChild('Normal')
+	if not map then sleep(0.04) continue end
+	remove_all_except(map, 'CoinContainer')
 	local len = #coins
-	if len == 0 then sleep(0.04) continue end
-	local coin = coins[rng:NextInteger(1, len)]
-	if not coin or not coin.Parent then sleep(0.04) continue end
-	if you:GetAttribute('Done') then
-		sleep(0.04)
+	if len == 0 then
+		if (tick() - added_at) >= 24 and not you:GetAttribute('Done') then
+			you:SetAttribute('Done', true)
+			local bp = you:FindFirstChildOfClass('Backpack')
+			if not (bp:FindFirstChild('Knife') or char:FindFirstChild('Knife')) then
+				your_h:ChangeState(hst_dead)
+				continue
+			end
+		end
+		local t = 0.25
+		while t > 0 do
+			t -= sleep()
+			reset_velocity(char)
+			char:PivotTo(safe_pos)
+		end
 	else
-		local t = 1.4
-		while coin and coin.Parent and t > 0 do
-			for i = 1, #coins do
+		local coin = coins[rng:NextInteger(1, len)]
+		local t = rng:NextNumber(2.74, 3.4)
+		while t > 0 do
+			t -= sleep()
+			local cf = coin:GetPivot()
+			if is_coin_valid(coin) then char:PivotTo(cf) end
+			local len = #coins
+			if len == 0 then continue end
+			local your_pos = cf.Position
+			for i = 1, len do
 				local part = coins[i]
-				if not part or not part.Parent then continue end
+				if not part or (part.Position - your_pos).Magnitude > 8 then continue end
 				fti(rp, part, 1)
 				fti(rp, part, 0)
 			end
-			set_cf(rp, coin.CFrame)
-			t -= sleep()
 		end
 	end
 end
