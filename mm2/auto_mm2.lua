e81d2a65673c06ecdfd65ec1b5a162751665a0fc
Absolutely No-one <158255806+cavasatchik571@users.noreply.github.com>
2024-07-28 19:28:16 +0300

    Update auto_mm2.lua

--- mm2/auto_mm2.lua
+++ mm2/auto_mm2.lua
@@ -419,7 +419,7 @@ while true do
 	else
 		local coin = best_coin()
 		local dist = if prev_coin then (coin.Position - prev_coin.Position).Magnitude else 384
-		local t = if dist >= 384 then 1.64 else dist ^ 0.5 / 1.64
+		local t = if dist >= 384 then 1.44 else dist ^ 0.6 / 1.64
 		prev_coin = coin
 
 		while char and char.Parent and t > 0 do
